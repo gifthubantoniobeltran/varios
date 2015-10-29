5472908b80b9ec1e1eea1c617c8cf548bb927d99
antonio <antonio@biruseto>
2015-10-29 10:10:27 +0100

    empezando proyecto beta

---
+++ ProyectoBeta/src/proyectobeta/ProyectoBeta.java
@@ -0,0 +1,14 @@
+
+package proyectobeta;
+
+/**
+ *
+ * @author antonio
+ */
+public class ProyectoBeta {
+
+    public static void main(String[] args) {
+        // Proyecto Beta empezado
+    }
+    
+}
